2c984b5e7a3018a8c1c7cd07fbbefcbb423356f9
LxxHB <109883014+LxxHB@users.noreply.github.com>
2024-03-06 00:42:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Unicos RP","discord.gg/2WAgUj3GYe"];
 
 
 let currentIndex = 0;
